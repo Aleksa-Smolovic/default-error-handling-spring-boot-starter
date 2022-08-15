fe70e233344875181700029442a609d851a59ed4
Aleksa <aleksa.smolovic@amplitudo.me>
2022-08-15 11:26:30 +0200

    - added EntityNotFoundException logic

--- src/main/java/com/xlea/me/defaulterrorhandling/error/ExceptionTranslator.java
+++ src/main/java/com/xlea/me/defaulterrorhandling/error/ExceptionTranslator.java
@@ -2,6 +2,9 @@ package com.xlea.me.defaulterrorhandling.error;
 
 import com.xlea.me.defaulterrorhandling.dto.ErrorResponse;
 import com.xlea.me.defaulterrorhandling.exception.BadActionException;
+import com.xlea.me.defaulterrorhandling.exception.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,17 +15,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ExceptionTranslator extends ResponseEntityExceptionHandler {
 
+    private final Logger log = LoggerFactory.getLogger(ExceptionTranslator.class);
+    private static final String EXCEPTION_LOGGER_PREFIX = "Exception: {}";
+
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleGenericException(Exception exception, NativeWebRequest request) {
-//        log.debug("Exception {}", (Object) exception);
+        log.debug(EXCEPTION_LOGGER_PREFIX, exception, exception);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse("HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()", exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
     }
 
     @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException exception, NativeWebRequest request) {
+        log.debug(EXCEPTION_LOGGER_PREFIX, exception, exception);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse("entity-missing", exception.getMessage(), HttpStatus.NOT_FOUND.value()));
+    }
+
+    @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleBadActionException(BadActionException exception, NativeWebRequest request) {
-//        log.debug("Exception {}", (Object) exception);
+        log.debug(EXCEPTION_LOGGER_PREFIX, exception, exception);
         HttpStatus httpStatus = HttpStatus.resolve(exception.getStatus());
         if (httpStatus == null)
             httpStatus = HttpStatus.BAD_REQUEST;
@@ -30,10 +44,5 @@ public class ExceptionTranslator extends ResponseEntityExceptionHandler {
                 .status(httpStatus)
                 .body(new ErrorResponse(exception.getCode(), exception.getDescription(), exception.getStatus()));
     }
-
-    // TODO unauthorized exception
-    // TODO unauthorized exception
-    // TODO entity not found exception
-    // TODO logger
 
 }
--- src/main/java/com/xlea/me/defaulterrorhandling/exception/EntityNotFoundException.java
+++ src/main/java/com/xlea/me/defaulterrorhandling/exception/EntityNotFoundException.java
@@ -1,5 +1,17 @@
 package com.xlea.me.defaulterrorhandling.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-    // TODO
+
+    private String identifier;
+    private String name;
+
+    @Override
+    public String getMessage() {
+        return String.format("Entity '%s' with identifier: '%s' was not found", name, identifier);
+    }
+
+    public EntityNotFoundException(String identifier, String name) {
+        this.identifier = identifier;
+        this.name = name;
+    }
 }
